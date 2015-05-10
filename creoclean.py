d805a277d2178649e1f4203b4752a3196a402e90
Roland Smith <rsmith@xs4all.nl>
2015-05-10 13:12:20 +0200

    Changed to clean named directories.
    Additionally:
    - Fixed program name in several places.
    - Changed version to 0.3.0.
    - Separated the cleaning into a function.

--- creoclean.py
+++ creoclean.py
@@ -4,14 +4,15 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2015-05-07 18:29:17 +0200
-# Last modified: 2015-05-10 10:59:06 +0200
+# Last modified: 2015-05-10 13:06:53 +0200
 #
 # To the extent possible under law, R.F. Smith has waived all copyright and
-# related or neighboring rights to cadclean.py. This work is published
+# related or neighboring rights to creoclean.py. This work is published
 # from the Netherlands. See http://creativecommons.org/publicdomain/zero/1.0/
 
-"""Cleans up Creo versioned files in the current working directory.  Removes
-all versions except the last one, and renamed that to version 1."""
+"""Cleans up Creo versioned files in the named diratories or in the current
+working directory.  Removes all versions except the last one, and renames that
+to version 1."""
 
 import argparse
 import logging
@@ -19,24 +20,26 @@ import os
 import re
 import sys
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 
 def main(argv):
     """
-    Entry point for cadclean.
+    Entry point for creoclean.
 
     Arguments:
-        param argv: command line arguments
+        argv: command line arguments
     """
     dr = "dry run; show what would be done but don't delete files"
-    opts = argparse.ArgumentParser(prog='cadclean', description=__doc__)
+    opts = argparse.ArgumentParser(prog='creoclean', description=__doc__)
     opts.add_argument('-d', dest='dry_run', action="store_true", help=dr)
     opts.add_argument('-v', '--version', action='version',
                       version=__version__)
     opts.add_argument('--log', default='warning',
                       choices=['debug', 'info', 'warning', 'error'],
                       help="logging level (defaults to 'warning')")
+    opts.add_argument("dirs", metavar='dir', nargs='*', default=[],
+                      help="one or more directories to process")
     args = opts.parse_args(argv)
     lfmt = '%(levelname)s: %(message)s'
     if args.dry_run:
@@ -45,7 +48,22 @@ def main(argv):
     else:
         logging.basicConfig(level=getattr(logging, args.log.upper(), None),
                             format=lfmt)
-    filenames = [e for e in os.listdir() if os.path.isfile(e)]
+    if not args.dirs:
+        args.dirs = ['.']
+    for directory in [d for d in args.dirs if os.path.isdir(d)]:
+        logging.info("cleaning in '{}'".format(directory))
+        cleandir(directory, args.dry_run)
+
+
+def cleandir(path, dry_run):
+    """Clean up Creo files in the named directory
+
+    Arguments:
+        path: The path of the directory to clean.
+        dry_run: Boolean to indicate a dry run.
+    """
+    filenames = [e for e in os.listdir(path) if
+                 os.path.isfile(os.path.join(path, e))]
     logging.info('found {} files'.format(len(filenames)))
     splits = [re.split('^(.*)\.([^\.]{3})\.([0-9]+)$', fn) for fn in filenames]
     splits = [s[1:-1] for s in splits if len(s) == 5]
@@ -66,13 +84,13 @@ def main(argv):
                 for n in numbers[:-1]:
                     fn = "{}.{}.{}".format(nm, ext, n)
                     logging.info("removing '{}'".format(fn))
-                    if not args.dry_run:
+                    if not dry_run:
                         os.remove(fn)
             oldfn = "{}.{}.{}".format(nm, ext, numbers[-1])
             newfn = "{}.{}.{}".format(nm, ext, 1)
             if oldfn != newfn:
                 logging.info("renaming '{}' to '{}'".format(oldfn, newfn))
-                if not args.dry_run:
+                if not dry_run:
                     os.rename(oldfn, newfn)
 
 
