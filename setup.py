177f8d4ed1e416d6e74788e62f293b436b7b34f7
Roland Smith <rsmith@xs4all.nl>
2022-01-23 17:53:37 +0100

    Updated setup script with uninstall option.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@
 # Copyright © 2020 R.F. Smith <rsmith@xs4all.nl>
 # SPDX-License-Identifier: MIT
 # Created: 2020-10-25T12:18:04+0100
-# Last modified: 2022-01-20T18:00:13+0100
-"""Script to install scripts for the local user."""
+# Last modified: 2022-01-23T17:52:46+0100
+"""Install scripts for the local user."""
 
 import os
 import shutil
@@ -14,45 +14,83 @@ import sys
 import sysconfig
 
 # What to install
-scripts = [("creoclean.py", ".py")]
+# The inner 2-tuples consist of the name of the script and the extension it
+# should have when installed on an ms-windows machine.
+SCRIPTS = (("creoclean.py", ".py"),)
 
-# Preparation
-if os.name == "posix":
-    destdir = sysconfig.get_path("scripts", "posix_user")
-    destdir2 = ""
-elif os.name == "nt":
-    destdir = sysconfig.get_path("scripts", os.name)
-    destdir2 = sysconfig.get_path("scripts", os.name + "_user")
-else:
-    print(f"The system '{os.name}' is not recognized. Exiting")
-    sys.exit(1)
-install = "install" in [a.lower() for a in sys.argv]
-if install:
-    if not os.path.exists(destdir):
-        os.makedirs(destdir)
-else:
-    print("(Use the 'install' argument to actually install scripts.)")
-# Actual installation.
-for script, nt_ext in scripts:
+
+def main():
+    cmd = None
+    if len(sys.argv) == 2:
+        cmd = sys.argv[1].lower()
+    dirs = dirnames()
+    if cmd == "install":
+        # Create primary installation directory if it doesn't exist.
+        if not os.path.exists(dirs[0]):
+            os.makedirs(dirs[0])
+            print(f"Created “{dirs[0]}”. Do not forget to add it to your $PATH.")
+    elif cmd == "uninstall":
+        pass
+    else:
+        print(f"Usage {sys.argv[0]} [install|uninstall]")
+    # Actual (de)installation.
+    for script, nt_ext in SCRIPTS:
+        names = destnames(script, nt_ext, dirs)
+        if cmd == "install":
+            do_install(script, names)
+        elif cmd == "uninstall":
+            do_uninstall(script, names)
+        else:
+            print(f"* '{script}' would be installed as '{names[0]}'")
+            if names[1]:
+                print(f"  or '{names[1]}'")
+
+
+def dirnames():
+    if os.name == "posix":
+        destdir = sysconfig.get_path("scripts", "posix_user")
+        destdir2 = ""
+    elif os.name == "nt":
+        destdir = sysconfig.get_path("scripts", os.name)
+        destdir2 = sysconfig.get_path("scripts", os.name + "_user")
+    else:
+        print(f"The system '{os.name}' is not recognized. Exiting")
+        sys.exit(1)
+    return destdir, destdir2
+
+
+def destnames(script, nt_ext, dest):
     base = os.path.splitext(script)[0]
     if os.name == "posix":
-        destname = destdir + os.sep + base
+        destname = dest[0] + os.sep + base
         destname2 = ""
     elif os.name == "nt":
-        destname = destdir + os.sep + base + nt_ext
-        destname2 = destdir2 + os.sep + base + nt_ext
-    if install:
-        for d in (destname, destname2):
-            try:
-                shutil.copyfile(script, d)
-                print(f"* installed '{script}' as '{destname}'.")
-                os.chmod(d, 0o700)
-                break
-            except (OSError, PermissionError, FileNotFoundError):
-                pass  # Can't write to destination
-        else:
-            print(f"! installation of '{script}' has failed.")
+        destname = dest[0] + os.sep + base + nt_ext
+        destname2 = dest[1] + os.sep + base + nt_ext
+    return destname, destname2
+
+
+def do_install(script, dest):
+    for d in dest:
+        try:
+            shutil.copyfile(script, d)
+            print(f"* installed '{script}' as '{d}'.")
+            os.chmod(d, 0o700)
+            break
+        except (OSError, PermissionError, FileNotFoundError):
+            pass  # Can't write to destination
     else:
-        print(f"* '{script}' would be installed as '{destname}'")
-        if destname2:
-            print(f"  or '{destname2}'")
+        print(f"! installation of '{script}' has failed.")
+
+
+def do_uninstall(script, dest):
+    for d in dest:
+        try:
+            os.remove(d)
+            print(f"* removed '{d}'")
+        except FileNotFoundError:
+            pass  # path doesn't exist
+
+
+if __name__ == "__main__":
+    main()
